dfa78270d8e6d5ab04ec6a9d04f171d429507284
AnthonySaillant <2354779@csfoy.ca>
2025-02-25 15:13:03 -0500

    Setup vue
    installation plus cleanup

---
+++ my-app/src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap";
+</script>
+
+<template>
+  <div>
+    <h1>oui</h1>
+  </div>
+</template>
+
+<style scoped></style>
